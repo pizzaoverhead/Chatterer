56456236ff2ae9e8586d9197682f409627c15e6f
Athlonic <athlonic.trash@live.fr>
2015-05-26 22:51:21 +0200

    Made hide_all_windows a global setting
    Avoid hiding GUI when switching per_vessel_setting On/Off in some sit.

--- Chatterer/Settings.cs
+++ Chatterer/Settings.cs
@@ -96,6 +96,7 @@ namespace Chatterer
             plugin_settings_node = new ConfigNode();
             plugin_settings_node.name = "SETTINGS";
             plugin_settings_node.AddValue("debugging", debugging);
+            plugin_settings_node.AddValue("hide_all_windows", hide_all_windows);
             plugin_settings_node.AddValue("use_vessel_settings", use_vessel_settings);
             plugin_settings_node.AddValue("useBlizzy78Toolbar", useBlizzy78Toolbar);
             plugin_settings_node.AddValue("http_update_check", http_update_check);
@@ -136,6 +137,7 @@ namespace Chatterer
                 if (debugging) Debug.Log("[CHATR] plugin_settings != null");
                 //Load settings specific to plugin.cfg
                 if (plugin_settings_node.HasValue("debugging")) debugging = Boolean.Parse(plugin_settings_node.GetValue("debugging"));
+                if (plugin_settings_node.HasValue("hide_all_windows")) hide_all_windows = Boolean.Parse(plugin_settings_node.GetValue("hide_all_windows"));
                 if (plugin_settings_node.HasValue("use_vessel_settings")) use_vessel_settings = Boolean.Parse(plugin_settings_node.GetValue("use_vessel_settings"));
                 if (plugin_settings_node.HasValue("useBlizzy78Toolbar")) useBlizzy78Toolbar = Boolean.Parse(plugin_settings_node.GetValue("useBlizzy78Toolbar"));
                 if (plugin_settings_node.HasValue("http_update_check")) http_update_check = Boolean.Parse(plugin_settings_node.GetValue("http_update_check"));
@@ -192,6 +194,7 @@ namespace Chatterer
                 if (debugging) Debug.Log("[CHATR] plugin_defaults != null");
                 //Load settings specific to plugin.cfg
                 if (plugin_settings_node.HasValue("debugging")) debugging = Boolean.Parse(plugin_settings_node.GetValue("debugging"));
+                if (plugin_settings_node.HasValue("hide_all_windows")) hide_all_windows = Boolean.Parse(plugin_settings_node.GetValue("hide_all_windows"));
                 if (plugin_settings_node.HasValue("use_vessel_settings")) use_vessel_settings = Boolean.Parse(plugin_settings_node.GetValue("use_vessel_settings"));
                 if (plugin_settings_node.HasValue("useBlizzy78Toolbar")) useBlizzy78Toolbar = Boolean.Parse(plugin_settings_node.GetValue("useBlizzy78Toolbar"));
                 if (plugin_settings_node.HasValue("http_update_check")) http_update_check = Boolean.Parse(plugin_settings_node.GetValue("http_update_check"));
@@ -237,7 +240,6 @@ namespace Chatterer
         {
             node.AddValue("show_tooltips", show_tooltips);
             node.AddValue("main_window_pos", main_window_pos.x + "," + main_window_pos.y);
-            node.AddValue("hide_all_windows", hide_all_windows);
             node.AddValue("skin_index", skin_index);
             node.AddValue("active_menu", active_menu);
             node.AddValue("remotetech_toggle", remotetech_toggle);
@@ -462,7 +464,6 @@ namespace Chatterer
             }
 
             if (node.HasValue("show_tooltips")) show_tooltips = Boolean.Parse(node.GetValue("show_tooltips"));
-            if (node.HasValue("hide_all_windows")) hide_all_windows = Boolean.Parse(node.GetValue("hide_all_windows"));
             if (node.HasValue("skin_index")) skin_index = Int32.Parse(node.GetValue("skin_index"));
             if (node.HasValue("active_menu")) active_menu = Int32.Parse(node.GetValue("active_menu"));
             if (node.HasValue("remotetech_toggle")) remotetech_toggle = Boolean.Parse(node.GetValue("remotetech_toggle"));
