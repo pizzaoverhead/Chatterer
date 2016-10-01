533cb9c5de8532cc1c6119247fa7ae20a08ccac8
Athlonic <athlonic.trash@live.fr>
2016-10-01 20:22:02 +0200

    Removed useless "using ..." IDE told me so
    Compile went well ... need testing tho...

--- Chatterer/AudioFilters.cs
+++ Chatterer/AudioFilters.cs
@@ -22,7 +22,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
 namespace Chatterer
--- Chatterer/Settings.cs
+++ Chatterer/Settings.cs
@@ -21,8 +21,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
 namespace Chatterer
